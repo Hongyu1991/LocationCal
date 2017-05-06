b55dfa6c3b53bd5d0a991c639aef18ec2f052d38
Hongyu <hy2504@columbia.edu>
2017-05-06 03:17:02 -0400

    add the data filter feature

--- src/location/LocSystem.java
+++ src/location/LocSystem.java
@@ -19,13 +19,66 @@ public class LocSystem {
 //	private double x5 = 0;
 //	private double y5 = 0;
 
+	List<Point> records;
+	private double DIFFERENCE_RATE;
+	
 	public LocSystem() {
 		list = new ArrayList<>();
 		list.add(new Device(x1, y1));
 		list.add(new Device(x2, y2));
 		list.add(new Device(x3, y3));
-		//list.add(new Device(x4, y4));
+		//list.add(new Device(x4, y4))；
+		
+		records = new ArrayList<>();
 	}
+
+	//every time we get a new list of distances, we first calculate its location 
+	//then try to compare with the last records. 
+	//if the distance between the new calculated point and the last valid point is within a logical range,
+	//we keep the record.
+	//distance(newPoint, lastPoint) / (newPoint.index - lastPoint.index) < DIFFERENCE_RATE;
+	public Point getCurrentValidPoint() {
+		//first we get the new point
+		Point coor = calculate();
+		//if this is the first point
+		if (records.size() < 1) {
+			records.add(coor);
+		} else {
+			double dis = Point.distance(coor, records.get(0)) / (coor.index - records.get(0).index);
+			if (dis < DIFFERENCE_RATE) { //we renew the records list
+				records.add(0, coor); //insert it into the records
+				if (records.size() > 10) {
+					records.remove(10); //remove the 11-th element
+				}
+				filter();
+			} else if (records.size() == 1) {
+				//if we have only one record, but the new one is not consistent with the old one, 
+				//we replace the old one. 
+				records.remove(0); 
+				records.add(coor);
+			}
+		}
+		
+		return records.get(0);
+	}
+	
+	private void filter() {
+		double[] ratio = new double[] {1.0, 1.0/2, 1.0/4, 1.0/8, 1.0/16, 
+				1.0/32, 1.0/64, 1.0/128, 1.0/256, 1.0/512, 1.0/1024};
+		double x = 0.0;
+		double y = 0.0;
+		for (int i = 0; i < records.size() - 1; i++) {
+			x += records.get(i).x * ratio[i + 1];
+			y += records.get(i).y * ratio[i + 1];
+		}
+		int size = records.size();
+		x += records.get(size - 1).x * ratio[size - 1];
+		y += records.get(size - 1).y * ratio[size - 1];
+		
+		records.get(0).x = x;
+		records.get(0).y = y;
+	}
+	
 	
 	//calculate the signal location
 	public Point calculate() {
